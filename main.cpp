01030874b00eb6b0292e1d062df84bb34aa987a2
andrewkoro105 <andrewkoro105@gmail.com>
2023-05-23 19:53:33 +0300

    Cmake update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include "lib/openLib.hpp"
+#include <openLib/openLib.hpp>
 
 int main() {
-	openLib::DL dL{"./liblib", {"foo", "value"}};
+	openLib::DL dL{"./liblib"   , {"foo", "value"}};
 	std::cout << dL.getSim<int(*)(int, int)>("foo")(3, 3) << '\n' << dL.getSim<int>("value") << std::endl;
 	return 0;
 }
